c1f066471f4d3e28b04954b2ffe3ad1de8f7c0b9
yacinekedidi <kedidiyacine@gmail.com>
2020-05-14 03:54:24 +0100

    new

--- func3.c
+++ func3.c
@@ -127,28 +127,3 @@ void pchar(stack_t **st, unsigned int i)
 		free_all(*st);
 	}
 }
-
-/**
- * pstr - divides the top two elements of the stack.
- * @st: first node of the list.
- * @i: lenght of the list.
- *
- * Return: void.
- */
-
-void pstr(stack_t **st, unsigned int i)
-{
-stack_t *tmp = *st;
-
-	if (*st)
-	{
-		for (; tmp ; tmp = tmp->next)
-		{
-			if (tmp->n <= 0 || tmp->n > 127)
-				break;
-				printf("%c", tmp->n);
-		}
-
-	}
-	printf("\n");
-}
